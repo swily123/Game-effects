985fe2adeae49f79d5cf8d0b575e242965d92024
swily123 <92625491+swily123@users.noreply.github.com>
2025-08-19 00:59:07 +0500

    add water effect

---
+++ Game effects/Assets/Scripts/Water.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField] private GameObject _effect;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Vector2 contactPoint = other.ClosestPoint(transform.position);
+        Instantiate(_effect, contactPoint, Quaternion.identity);
+    }
+    
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     Physics2D.IgnoreCollision(other.collider, other.otherCollider);
+    //     Instantiate(_effect, other.GetContact(0).point, Quaternion.identity);
+    // }
+}
