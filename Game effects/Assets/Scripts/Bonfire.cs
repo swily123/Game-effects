daaa78facd319a447b54b5ebca95d1b4babbf82c
swily123 <92625491+swily123@users.noreply.github.com>
2025-08-19 03:17:45 +0500

    add interact to fire

---
+++ Game effects/Assets/Scripts/Bonfire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.TryGetComponent(out Fireable fireableObject))
+        {
+            fireableObject.OnEnter();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.TryGetComponent(out Fireable fireableObject))
+        {
+            fireableObject.OnExit();
+        }
+    }
+}
---
+++ Game effects/Assets/Scripts/Fireable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Fireable : MonoBehaviour
+{
+    [SerializeField] private GameObject _effect;
+
+    private const float Delay = 2;
+    private Coroutine _coroutine;
+    
+    public void OnEnter()
+    {
+        _effect.SetActive(true);
+    }
+
+    public void OnExit()
+    {
+        if(_coroutine != null)
+            StopCoroutine(_coroutine);
+        
+        _coroutine = StartCoroutine(StoppingEffect());
+    }
+
+    private IEnumerator StoppingEffect()
+    {
+        WaitForSeconds delay = new WaitForSeconds(Delay);
+        yield return delay;
+        _effect.SetActive(false);
+    }
+}
